0a551138ed2863c5442e37787aca711eafa64c5e
Corben McClintock <s544086@nwmissouri.edu>
2023-11-16 13:42:18 -0600

    Prediction and Correlation
    Created a correlation of movies based on the user ratings and gives the top 10 movies that are similar to the movie given.

--- Beginner Projects/Movie Correlation Project/Movie Correlation.py
+++ Beginner Projects/Movie Correlation Project/Movie Correlation.py
@@ -18,8 +18,32 @@ movie_titles.head()
 df = pd.merge(data, movie_titles, on='item_id')
 df.head()
 
-#Now we need to group the same movie entries
-#After we group by the title we are going to give them the average rating
-#Then we will put them in order from best to worst
+#Now we need to group the same movie entries using groupby()
+#After we group by the title we are going to give them the average rating using mean()
+#Then we will put them in order from best to worst using sort_values
 rating_and_no = pd.DataFrame(df.groupby('title')['rating'].mean().sort_values(ascending=False))
-rating_and_no
+
+
+#Now we are adding a column of the number of ratings
+#We will do this by using the groupby() function again but taking the count of all the ratings
+#Then we will sort by the number of ratings
+rating_and_no['no_of_ratings'] = df.groupby('title')['rating'].count()
+rating_and_no = rating_and_no.sort_values('no_of_ratings', ascending=False)
+
+
+#Creating a pivot table
+#Users go along the rows and movies go along the columns
+pt = df.pivot_table(index='user_id', columns='title', values='rating')
+pt.head()
+
+#Here is the prediction part of the code
+test_movie = input('Enter movie name-->')
+
+movie_vector = pt[test_movie].dropna()
+similar_movies = pt.corrwith(movie_vector)
+
+corr_df = pd.DataFrame(similar_movies, columns=['Correlation'])
+corr_df = corr_df.join(rating_and_no['no_of_ratings'])
+
+corr_df = corr_df[corr_df['no_of_ratings']>100].sort_values('Correlation', ascending=False).dropna()
+print(corr_df.head(10))
